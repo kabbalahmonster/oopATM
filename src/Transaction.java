4e666ab838edc20807ac4df6d2b6cadc387f1208
ShowalterN <w0419574@nscc.ca>
2019-04-22 18:04:34 -0300

    initialized project. created classes. coded transaction class.

---
+++ src/Transaction.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Transaction {
+	
+	Date dateTime;
+	String description;
+	Double amount;
+	
+	public Transaction(Date myDateTime, String myDescription, Double myAmount) {
+		dateTime = myDateTime;
+		description = myDescription;
+		amount = myAmount;
+	}
+	
+	
+	//------------------------------------ Gets
+	
+	public Date getDateTime() {
+		return dateTime;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	//----------------------------------- Sets
+	
+	public void setDateTime(Date myDateTime) {
+		dateTime = myDateTime;
+	}
+	
+	public void setDescription(String myDescription) {
+		description = myDescription;
+	}
+	
+	public void setAmount(Double myAmount) {
+		amount = myAmount;
+	}
+}
